9447ec6e404c08d073530d67703597c8710552ef
izumin5210 <masayuki@izumin.info>
2015-07-04 04:02:54 +0900

    Implement sample app

--- server/src/websocketserver.js
+++ server/src/websocketserver.js
@@ -1,16 +1,27 @@
 import Server from "socket.io";
 
-class WebSocketServer {
+export default class WebSocketServer {
   constructor(port) {
     this.io = new Server();
     this.io.on("connection", this.onConnect.bind(this));
+  }
+
+  listen(port) {
     this.io.listen(port);
   }
 
   onConnect(socket) {
+    console.log("connect!");
+    socket.on("chat message", this.onChatMessage.bind(this));
     socket.on("disconnect", this.onDisconnect.bind(this));
   }
 
   onDisconnect() {
+    console.log("disconnect...");
+  }
+
+  onChatMessage(msg) {
+    console.log(`received: ${msg}`);
+    this.io.emit("chat message", msg);
   }
 }
