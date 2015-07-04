9720d773a52f967fb0f16b86a53bfe4214cffd2c
三上航人 <mkm@shuto.info>
2015-07-04 14:20:26 +0900

    透明度変更

--- client/app/libs/update.js
+++ client/app/libs/update.js
@@ -14,7 +14,7 @@ function drewer(){
     rotate(10);
     var x = map(i, 0, data.length, 0, width);
     var h = - width/2 + map(data[i], 0, 255, width/2, 0);
-    var c = color(data[i]*2.5, 60, 20, data[i]/100);
+    var c = color(data[i]*2.5, 60, 20, data[i]/50);
     fill(c);
     rect(width/2,i*10-30, h, 10);
     rect(width/2,30+height-(i*10), -h, 10);
